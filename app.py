d7e2d013dc6d5b09a224b8b2ad08253a3616bbed
circlecoins <158820911+circlecoins@users.noreply.github.com>
2024-02-05 22:41:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+from main import open_instagram_and_click_account
+import threading
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', methods=['POST'])
+def run_script():
+    # Получаем данные из тела запроса в формате JSON
+    data = request.json
+    username = data['username']
+    password = data['password']
+    account_name = data['account_name']
+
+    # Запускаем скрипт Selenium в отдельном потоке
+    threading.Thread(target=open_instagram_and_click_account, args=(username, password, account_name)).start()
+
+    # Отправляем ответ, что скрипт запущен
+    return {"status": "Script is running"}
